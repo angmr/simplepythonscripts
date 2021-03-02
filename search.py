fd810324ebeff28164b30c43156cadebd50f5162
angmr <79375183+angmr@users.noreply.github.com>
2021-03-02 12:02:05 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 """
-This is a program that generates a list of random numbers, the user can then search whether or not a number appears in the list, and if so how many times.
+This is a program that generates a list of random numbers, the user can then search whether or not a number appears in the list, and if so how many times and where.
 Finally a message is printed letting the user know the result.
 """
 
@@ -27,3 +27,7 @@ if number in values:
     print(f"The value {number} appears in the list {times} time/s.")
 else:
     print(f"The value {number} is not contained in the list.")
+
+for i in range(len(values)):
+    if values[i] == number:
+        print(f"The value appears in place {i}")
