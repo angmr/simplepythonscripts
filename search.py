e01f864a9652290940e25c014aa2bc73576d59de
angmr <79375183+angmr@users.noreply.github.com>
2021-03-02 11:46:59 +0200

    Create search.py

---
+++ search.py
@@ -0,0 +1,24 @@
+from random import seed
+from random import randrange
+
+values = []
+
+for i in range(10):
+    value = randrange(11)
+    values.append(value)
+
+print(values)
+number = input("Enter the number you want to search for: ")
+
+number = number.strip()
+
+while not number.isdigit():
+    number = input("The value you enter must be a number: ")
+
+number = int(number)
+
+if number in values:
+    times = values.count(number)
+    print(f"The value {number} appears in the list {times} time/s.")
+else:
+    print(f"The value {number} is not contained in the list.")
